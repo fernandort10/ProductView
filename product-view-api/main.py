3c24521e5ce343f482a6171baf1db4643fcc5f7c
Fernando Reyes <73537128+fernandort10@users.noreply.github.com>
2022-09-15 02:42:17 -0400

    fix: cors conflict

--- product-view-api/main.py
+++ product-view-api/main.py
@@ -16,15 +16,9 @@ from userModel import Users
 
 app = FastAPI()
 
-origins = ['https://localhost:3000']
+origins = ["http://localhost:3000"]
 
-app.add_middleware(
-    CORSMiddleware, 
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
-)
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"])
 
 #Products
 
